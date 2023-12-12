b49e5eb6cb22f2c58b0365db1db5dcaddd422f75
Johan Gangsås Hole <johan@devotta.no>
2023-12-12 15:47:42 +0100

    test: explore the different camelContext methods
    Explored the different  methods to control the CamelContext lifecycle:
    start(), stop(), suspend(), resume()
    
    Created some simple example routes with File component

--- chapter1/file-copy/src/main/java/camelinaction/FileCopierWithCamel.java
+++ chapter1/file-copy/src/main/java/camelinaction/FileCopierWithCamel.java
@@ -1,27 +1,74 @@
 package camelinaction;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class FileCopierWithCamel {
+public class FileCopierWithCamel
+{
 
-    public static void main(String args[]) throws Exception {
-        // create CamelContext
+    public static void main( String args[] )
+        throws
+        Exception
+    {
+        final Logger LOGGER = LoggerFactory.getLogger( FileCopierWithCamel.class );
+
         CamelContext context = new DefaultCamelContext();
 
-        // add our route to the CamelContext
-        context.addRoutes(new RouteBuilder() {
-            public void configure() {
-                from("file:data/inbox?noop=true").to("file:data/outbox");
+        context.addRoutes( new RouteBuilder()
+        {
+            public void configure()
+            {
+                from( "file:data/inbox?noop=true" )
+                    .routeId( "Copy File From Inbox" )
+                    .log( LoggingLevel.INFO, LOGGER, "Copying from inbox..." )
+                    .to( "file:data/outbox" )
+                    .log( LoggingLevel.INFO, LOGGER, "Added to outbox..." );
             }
-        });
+        } );
 
-        // start the route and let it do its work
         context.start();
-        Thread.sleep(10000);
+        Thread.sleep( 5000 );
+        context.addRoutes( new RouteBuilder()
+        {
+            @Override
+            public void configure()
+                throws
+                Exception
+            {
+                from( "file:data/outbox?noop=true" )
+                    .log( LoggingLevel.INFO, LOGGER, "Hello World" )
+                    .setBody( simple( "This is a new body. The previous body was ${body}" ) )
+                    .to( "file:data/newBody" );
+            }
+        } );
+        context.addRoutes( new RouteBuilder()
+        {
+            @Override
+            public void configure()
+                throws
+                Exception
+            {
+                from( "timer:mockDataTimer?period=1000" )
+                    .routeId( "Generate Mock Data Route Builder" )
+                    .setBody().constant( "Hello world" )
+                    .log( LoggingLevel.INFO, LOGGER, "Generating mock data file" )
+                    .to( "file:data/mockFiles?fileName=mockData_${date:now:yyyyMMddHHmmss}.txt" );
+            }
+        } );
+        context.start();
+        context.suspendRoute( "Generate Mock Data Route Builder" );
+        Thread.sleep( 5000 );
+        context.suspend();
+        Thread.sleep( 5000 );
+        // should be no new mock files in the inbox...
+        context.resume();
+        context.resumeRoute( "Generate Mock Data Route Builder" );
+        Thread.sleep( 3000 );
 
-        // stop the CamelContext
         context.stop();
     }
 
